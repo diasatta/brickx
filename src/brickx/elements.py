cbd59978bb115b48344b7bd2b6026277ceab16f0
Said Atta <saidattaenp@yahoo.fr>
2025-06-24 19:28:34 +0100

    feat: create a new namespace for components

--- src/brickx/elements.py
+++ src/brickx/elements.py
@@ -1116,37 +1116,3 @@ class Wbr(Element):
   inline: bool = True
 
 #endregion
-
-class Document(Root):
-  """
-  Represents a html document. This is a helper element equivalent to:
-  ```
-  <!DOCTYPE html>
-  <html>
-    <head></head>
-    <body></body>
-  </html>
-  ```
-
-  Argument:
-    `nodes`: children to pass to the `body` element.
-
-  Properties:
-    - `doctype`: returns the `doctype` element
-    - `html`: returns the `html` element
-    - `head`: returns the `head` element
-    - `body`: returns the `body` element
-  """
-
-  tag_name: str = "document"
-  inline: bool = False
-
-  def __init__(self, *nodes: Child) -> None:
-    super().__init__()
-
-    self.doctype = Doctype()
-    self.html = Html()
-    self.body = Body(*nodes)
-    self.head = Head()
-
-    self.doctype + (self.head + self.body >> self.html) >> self
