21d8ff3bf7af6b32f9c6690a30c42c58a308edc5
Said Atta <saidattaenp@yahoo.fr>
2025-06-24 19:21:14 +0100

    feat: drop native support for htmx attributes
    Custom attributes can be worked with using user-defined TypedDict to benefit from type checking and autocompletion. The same pattern can be applied to data and aria attributes.

--- src/brickx/attrs.py
+++ src/brickx/attrs.py
@@ -764,44 +764,6 @@ class GlobalAttrs(TypedDict, total=False):
   translate: TranslateL
   virtualkeyboardpolicy: str
 
-  _: str
-  script: str
-  hx_boost: str
-  hx_confirm: str
-  hx_delete: str
-  hx_disable: str
-  hx_disabled_elt: str
-  hx_disinherit: str
-  hx_encoding: str
-  hx_ext: str
-  hx_get: str
-  hx_headers: str
-  hx_history_elt: str
-  hx_history: str
-  hx_include: str
-  hx_indicator: str
-  hx_inherit: str
-  hx_on: str
-  hx_params: str
-  hx_patch: str
-  hx_post: str
-  hx_preserve: str
-  hx_prompt: str
-  hx_push_url: str
-  hx_put: str
-  hx_replace_url: str
-  hx_request: str
-  hx_select_oob: str
-  hx_select: str
-  hx_swap_oob: str
-  hx_swap: str
-  hx_sync: str
-  hx_target: str
-  hx_trigger: str
-  hx_validate: str
-  hx_vals: str
-  hx_vars: str
-
 class AAttrs(GlobalAttrs, total=False):
   download: str
   href: str
